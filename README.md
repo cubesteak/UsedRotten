237519c546dfb3422c4d021e4c8fb40369a7f061
cubesteak <cubesteak@cubesteak.net>
2014-03-17 02:10:23 -0500

    Added crafting permission.

--- README.md
+++ README.md
@@ -9,11 +9,16 @@ It adds:
  - 4 rotten flesh in a 2x2 pattern on a crafting table will make 1 leather.
  - Rotten Flesh is also now cookable in a furnace to make leather.
 
+Permission:
+ - The permission UsedRotten.Crafting allows people to craft flesh to leather.
+ - Working on a furnace permission, but none yet.
+
+
 Written for CraftBukkit Beta Build (1.7.2-R0.3)
  
 To do:
 
- - Add permissions
+ - Add furnace permissions
  - Add crafting variations for flexibility
  - See if I can get a furnace to consume more than 1 flesh per leather created.
  - Allow one or both to be used via configuration file
--- UsedRotten/src/net/cubesteak/bukkit/UsedRotten/UsedRotten.java
+++ UsedRotten/src/net/cubesteak/bukkit/UsedRotten/UsedRotten.java
@@ -1,21 +1,31 @@
 package net.cubesteak.bukkit.UsedRotten;
 
 import org.bukkit.Server;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Material;
 
+import ro.thehunters.digi.recipeUtil.RecipeUtil;
 
-public class UsedRotten extends JavaPlugin {
+public class UsedRotten extends JavaPlugin implements Listener {
 
+	private ShapedRecipe leather;
+	
 	@Override
 	public void onEnable() {
 		getLogger().info("Ready to have UsedRotten!");
 		Server server = this.getServer();
+
+		server.getPluginManager().registerEvents(this, this);
 		
-		ShapedRecipe leather = new ShapedRecipe(new ItemStack(Material.LEATHER));
+		leather = new ShapedRecipe(new ItemStack(Material.LEATHER));
 		leather.shape("FF","FF");
 		leather.setIngredient('F', Material.ROTTEN_FLESH);
 		server.addRecipe(leather);
@@ -28,5 +38,36 @@ public class UsedRotten extends JavaPlugin {
 	public void onDisable() {
 		getLogger().info("Thank you for having UsedRotten!");
 	}
+	
+	 @EventHandler
+	    public void preCraft(PrepareItemCraftEvent event) // event is triggered when a recipe is found after placing ingredients
+	    {
+			// Storing the equality result because it's not cheap to compare recipes.
+	        // I'm doing this because I'm using it twice, if you're using it only once you don't need to store it.
+	        boolean equal = RecipeUtil.areEqual(event.getRecipe(), leather);
+	        
+	        // confirmation message of event triggering and recipe equality
+	        System.out.print("(debug) recipes equal = " + equal);
+	        
+	        if(equal)
+	        {
+	            HumanEntity human = event.getView().getPlayer();
+	            
+	            // check if crafter has permission...
+	            if(!human.hasPermission("UsedRotten.Crafting"))
+	            {
+	                // basically cancels the event
+	                event.getInventory().setResult(null);
+	                
+	                // need to check because it could be NPCs or something
+	                if(human instanceof Player)
+	                {
+	                    Player player = (Player)human;
+	                    player.sendMessage("Need permission to craft this!");
+	                }
+	            }
+	        }
+	    }
+	
 	
 }
